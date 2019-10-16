e42be9b53d075a69c1c0500b6899a962ef5b6e70
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-10-16 10:11:10 -0400

    met all specs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  CATEGORIES = Post.all.map(&:category)
+  CATEGORIES << "Fiction"
+  CLICKBAIT = ["Won't Believe", "Secret", "Top ", "Guess"]
+  validates :title, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :summary, length: { maximum: 100 }
+  validates :category, inclusion: { in: CATEGORIES }
+  validate :is_clickbait
+
+  def is_clickbait
+    cond = CLICKBAIT.any? do |phrase|
+      title.include?(phrase) unless title.nil?
+    end
+
+    unless cond
+      errors.add(:title, "Not clickbait!")
+    end
+  end
+  
 end
