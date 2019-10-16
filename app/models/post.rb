2dbba30939f3f80e3a7b68210023165f5a3731da
dan1mal <dreamer@panix.com>
2019-10-16 16:52:58 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250, too_short: "Minimum character count is 250, you currently have %{count}" }
+    validates :summary, length: { maximum: 250, too_long: "Maximum character count is 250, you currently have %{count}" }
+    validates :category, inclusion: { in: %w{Fiction Non-Fiction}}
+    validate :title, :is_click_bait?
+
+    CB_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+    ]
+
+    def is_click_bait?
+        if CB_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
