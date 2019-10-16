e864d1703d58a79a79971cb031660a1b0479b1fc
jz0001 <jinming.zhang.92@gmail.com>
2019-10-16 14:04:38 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    validates :title, length: {minimum: 1}
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    include ActiveModel::Validations
+    validates_with TitleValidator
 end
